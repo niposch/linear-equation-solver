16192fb1704a88ad15f19eff1eade4d6fe0eda65
niposch <niposch@gmail.com>
2022-10-11 11:25:52 +0200

    Created equation input component

---
+++ src/components/EquationInput/EquationInput.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './EquationInput.module.css';
+
+interface EquationInputProps {}
+
+const EquationInput: FC<EquationInputProps> = () => (
+  <div className={styles.EquationInput}>
+    EquationInput Component
+  </div>
+);
+
+export default EquationInput;
